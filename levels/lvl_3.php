c59593636c149896204e2c24e99afed04efbae55
toniginebro <antonillombart@ginebro.cat>
2014-04-28 12:55:24 +0200

    zsc

--- levels/lvl_3.php
+++ levels/lvl_3.php
@@ -18,7 +18,7 @@
        <div id="plantilla">
             <div id="portada" class="no_portada">
                 <span class="nivel_pantalla">3</span>
-                <span style="font-family:'Arial';">Fes clic en <span name="si">el més gran</span>.</span>
+                <span style="font-family:'Arial';">Fes clic sobre <span name="si">el més gran</span>.</span>
             </div>
      
             <div id="marcadores" style="width:100%; height: 47%; margin-left: 5%;">
@@ -37,7 +37,7 @@
                 <ul style="list-style: none outside none;">
                     <li class="bt3" name="no" style="float: left;display: block; width: 44%;height: 50%;">
                         <div class="respuesta" >
-                            <span class="size">El més gran</span>
+                            <span class="size" style="font-size:40pt;">Formiga</span>
          
 
                         </div>
@@ -45,7 +45,7 @@
                     
                     <li class="bt4" name="no" style="float: left;display: block; width: 50%;height: 50%;">
                         <div class="respuesta">
-                            <span class="size">Elefent gegant</span>
+                            <span class="size">Leo Messi</span>
                          
                         </div>
                     </li>
