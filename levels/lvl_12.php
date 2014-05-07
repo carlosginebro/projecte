78fde6cc756a28439c2a3df13caa8532bf0adf19
carlosginebro <carlosmasegosa@gmail.com>
2014-05-07 18:01:31 +0200

    xs

--- levels/lvl_12.php
+++ levels/lvl_12.php
@@ -1,15 +1,20 @@
 <html>
 <head>
-        <meta charset="ISO-8859-15" />
+       <meta charset="utf-8" />
         <?php
             require_once 'headlvls.php';
             require_once '../funcions.php';
-            require_once '../animacio_correcte.php';
+            require_once '../animacio_perdre_vida.php'; 
         ?>
+       
        <script src="../js/nivel12.js"></script>
 	   <style>
 		img{
 			cursor : none;
+		}
+		
+		#lab{
+			display: none;
 		}
 		
 		
@@ -26,22 +31,24 @@
             </div>
 	<!-- -->
             <div id="marcadores" style="width:100%; height: 64%; margin-left: 35%;">
-                <img src ="../css/galery/labfacil.jpg" alt="labFacil" id="ima"usemap="#laberinto" />
+				<div id="ratoli">
+					<img src ="../css/galery/raton.jpg" alt="raton"   onmouseover="ratoli()" />
+			</div>
+				
+			<div id="lab">
+					<img src ="../css/galery/labfacil.jpg" alt="labFacil"   usemap="#laberinto" />		
+			</div>
 				<map name="laberinto">
-					<area shape="rect" coords="5,33,6,286" onmouseover="linia()" />
-					<area shape="rect" coords="5,286,259,287" onmouseover="linia()" />
-					<area shape="rect" coords="5,234,56,236" onmouseover="linia()" />
-					<area shape="rect" coords="57,83,54,31" onmouseover="linia()" />
-					<area shape="rect" coords="55,31,259,35" onmouseover="linia()" />
-					<area shape="rect" coords="54,134,57,236" onmouseover="linia()" />
-					<area shape="rect" coords="104,32,109,134" onmouseover="linia()" />
-					<area shape="rect" coords="104,183,108,286" onmouseover="linia()" />
-					<area shape="rect" coords="104,183,209,187" onmouseover="linia()" />
-					<area shape="rect" coords="156,234,260,238" onmouseover="linia()" />
-					<area shape="rect" coords="156,81,209,86" onmouseover="linia()" />
-					<area shape="rect" coords="155,134,159,186" onmouseover="linia()" />
-					<area shape="rect" coords="206,82,210,187" onmouseover="linia()" />
-					<area shape="rect" coords="256,31,260,238" onmouseover="linia()" />				
+					<area shape="rect" coords="52,31,8,3" onmouseover="comensalvl()" />
+					
+					<div id="porv">
+					
+					</div>
+					
+					<area shape="rect" coords="260,231,299,230" onmouseover="linia()"  />
+					<area shape="rect" coords="299,231,297,286" onmouseover="linia()"  />
+					<area shape="rect" coords="298,284,259,287" onmouseover="linia()"  />
+					<area shape="rect" coords="260,235,299,287" onmouseover="conseguit()"  />					
 				</map>
             </div>
             <div id="vidas">
@@ -52,7 +59,7 @@
             </div>
                 
             </div>
-<input type="text" value="3" id="getLvl" />
+<input type="text" value="12" id="getLvl" />
 <input type="text" value="<?php echo $_SESSION['vides'];?>" class="cHiden" id="videsbd" />
 <input type="text" value="<?php echo $_SESSION['comodins']; ?>" class="cHiden" id="getComodins" />
 
