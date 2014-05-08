9834ed2499ea5df76b696f1eed6f1cb487e0689d
carlosginebro <carlosmasegosa@gmail.com>
2014-05-08 19:56:32 +0200

    sesion idioma

--- funcions.php
+++ funcions.php
@@ -12,23 +12,23 @@
     function getPregunta($nivell){
         $conec= cone();
 		
-        $sql = "select * from preguntes_$_SESSION[idioma] where nivell_pregunta =".$nivell;
+        $sql = "select * from preguntes where nivell_pregunta =".$nivell;
         $consulta = $conec->query($sql);
         $random = rand(0,3);
 
         $consulta->data_seek($random);
         $reg = $consulta->fetch_array();
-        $pregunta_final = $reg['pregunta'];
+        $pregunta_final = $reg['pregunta_ca'];
 	$id_pregunta = $reg['id_pregunta'];	
 	
-        $sql = "select * from respostes_$_SESSION[idioma] where fk_pregunta = $id_pregunta";
+        $sql = "select * from respostes where fk_pregunta = $id_pregunta";
         
         $consulta = $conec->query($sql);
         $respostes = array();
         $cont = 0;
         
         while($reg = $consulta->fetch_array()){
-            $respostes[$cont]="#".$reg['correcte']."#".$reg['resposta'];
+            $respostes[$cont]="#".$reg['correcte']."#".$reg['resposta_ca'];
             $cont++;
 //            $pregunta_final.="#".$reg['correcte']."#".$reg['resposta'];
         }
