5a2fb76570567f8b9d7f0ca187339dcfa3693f34
toniginebro <antonillombart@ginebro.cat>
2014-05-12 17:47:21 +0200

    hg

--- gameover.php
+++ gameover.php
@@ -5,7 +5,7 @@
         ?>
     </head>
     <body id="container" >
-        <div id='plantilla' style="background-image: url('css/galery/gameover.png'); display:block; width:100%; height:100%; margin:0 0; background-repeat: no-repeat; background-size: 100% 100%;">
+        <div id='plantilla' style="background-image: url('css/galery/gameover.jpg'); display:block; width:100%; height:100%; margin:0 0; background-repeat: no-repeat; background-size: 100% 100%;">
             <span id="try_again" class="css_gameover">Un altre cop!</span>
             <span id="sortir_game" class="css_gameover">Em rendeix-ho...</span>  
         </div>
