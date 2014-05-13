7b13676d03ace679d1c3855d826bf127fa4951e0
toniginebro <antonillombart@ginebro.cat>
2014-05-13 14:29:59 +0200

    nn

---
+++ android/crearUsu.php
@@ -0,0 +1,54 @@
+<?php  
+    header('Content-Type: text/html; charset=utf-8');
+
+    $obj = json_decode(utf8_encode($_POST["json"]));
+
+    $host = 'localhost';
+        $usu = 'root';
+        $contra = '';
+        $bd = 'projecte';
+        $cone = new mysqli($host, $usu, $contra, $bd);
+
+    if($obj != null){
+        
+        $u = $obj->{'usuari'};
+        $p = $obj->{'password'};
+        $m = $obj->{'mail'};
+        
+        $sql = "select Id from usuari where Usuari = '$u'";
+        $consulta = $cone->query($sql);
+        
+        if ($reg = $consulta->fetch_array()){
+            echo "0"; //Usuari amb el mateix nom ja creat
+        }else{
+            
+            
+            //include("mail.php");  fer que envii mail
+            
+            $recovery = "";
+            
+            $recovery .= strlen($u);
+            $recovery .= strlen($p);
+            $recovery .= strlen($m);
+            $recovery .= substr($u, 0, 1);
+            $recovery .= substr($m, 0, 2);
+            
+            $sql = "insert into usuari(Usuari, Password, RecoveryName, Vides, Comodins, PuntuacioMax, Admin) values ('$u', '$p', '$recovery', 3, 0, 0, 0)";
+        
+            $cone->query($sql);
+            
+            mysqli_close($cone);
+            
+            echo "1";
+        }
+        
+    }
+        
+
+
+       
+ 
+	
+
+   
+?>
