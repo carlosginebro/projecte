64daf21da08f4d13e047bf2e4d9cb9efbb7223be
carlosginebro <carlosmasegosa@gmail.com>
2014-05-14 16:15:00 +0200

    z

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
         <script language="javascript" src="js/jquery-ui.js"></script>
         <script language="javascript" src="js/jquery.circulate.js"></script>     
         <script language="javascript" src="js/bootstrap.js"></script>
-        <script src="js/imposiblequiz.js" type="text/javascript"></script>
+        <script src="js/crazyuniverse.js" type="text/javascript"></script>
         <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css" />  
         <link rel="stylesheet" type="text/css" href="css/clean.css" /> 
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
--- levels/headlvls.php
+++ levels/headlvls.php
@@ -5,7 +5,7 @@
 <script language="javascript" src="../js/jquery.gridster.js"></script>
 <script language="javascript" src="../js/jquery.circulate.js"></script>     
 <script language="javascript" src="../js/bootstrap.js"></script>
-<script src="../js/imposiblequiz.js" type="text/javascript"></script>
+<script src="../js/crazyuniverse.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="../dist/css/bootstrap.css" />  
 <link rel="stylesheet" type="text/css" href="../css/clean.css" />
 <link rel="stylesheet" type="text/css" href="../css/jquery.gridster.css" />
