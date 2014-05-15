f6970b2b81e3fb30778498a104cf8a9c2ca26ab5
carlosginebro <carlosmasegosa@gmail.com>
2014-05-15 17:54:20 +0200

    mail

--- mail.php
+++ mail.php
@@ -8,19 +8,16 @@ $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;                             
 $mail->Port       = 587;
 $mail->Username = 'ctcrazyuniverse@gmail.com';      
-$mail->Password = 'projecte123'; //Algú esperava que poses la meva contrasenya?
+$mail->Password = 'projecte123'; 
 $mail->SMTPSecure = 'tls'; 
 $mail->SMTPDebug  = 0;                         
 
 $mail->From = 'ctcrazyuniverse@gmail.com';
 $mail->FromName = 'CrazyUniverse';
 $mail->addAddress("$email" , "$u");  
-/*$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');*/
 
 $mail->WordWrap = 50;                       
-//$mail->addAttachment('pas.txt');   
+  
 $mail->isHTML(true);                          
 
 $mail->Subject = 'Contrasenya i Password';
