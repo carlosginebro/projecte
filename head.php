ac14e23c57941164c96a4a9cdcb28cc233492d12
toniginebro <antonillombart@ginebro.cat>
2014-05-26 17:56:19 +0200

    ad

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 
-    
+        
         <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
         <script language="javascript" src="js/jquery-1.10.2.js"></script>
         <script language="javascript" src="js/jquery-ui.js"></script>
@@ -10,4 +10,5 @@
         <link rel="stylesheet" type="text/css" href="css/clean.css" /> 
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/crazyuniverse.css" />
-        
+        <!--<link rel="shortcut icon" type="image/x-icon" href="/css/galery/favicon.ico" />-->
+        <link id="page_favicon" href="css/galery/favicon.ico" rel="icon" type="image/x-icon" />
